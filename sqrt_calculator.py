0393c7a1ad32c9448c23db06a0448f6343c37584
amitie94 <aminaabdulazeez0@gmail.com>
2023-12-01 22:20:40 +0100

    add day1

---
+++ sqrt_calculator.py
@@ -0,0 +1,8 @@
+# import math module
+import math
+# define the integer value to the variable num1
+num1 = 36
+# use math.sqrt() function and pass the variable.
+result = math.sqrt(num1)
+# to print the square root of a given number n
+print(" Square root of  number 36 is : ", result)
